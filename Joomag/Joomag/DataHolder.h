06cd9b132f6c543f380dd112e0512619693f1d0b
Anatoli-Petrosyants <anatoli.petrosyants@sourcio2.com>
2013-07-21 01:10:07 +0400

    DataHolder Class
    Now it includes small parsing mechanism for testXML

---
+++ Joomag/Joomag/DataHolder.h
@@ -0,0 +1,13 @@
+//
+//  DataHolder.h
+//  Joomag
+//
+//  Created by Anatoli Petrosyants on 2013-07-21.
+//  Copyright (c) 2013 Joomag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataHolder : NSObject
+
+@end
---
+++ Joomag/Joomag/DataHolder.m
@@ -0,0 +1,13 @@
+//
+//  DataHolder.m
+//  Joomag
+//
+//  Created by Anatoli Petrosyants on 2013-07-21.
+//  Copyright (c) 2013 Joomag. All rights reserved.
+//
+
+#import "DataHolder.h"
+
+@implementation DataHolder
+
+@end
