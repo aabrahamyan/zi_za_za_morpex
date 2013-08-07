11c51932340fb6c85e2162ae131c53c86af0fb50
Anatoli-Petrosyants <anatoli.petrosyants@sourcio2.com>
2013-08-07 22:37:34 +0400

    Explore View iPad (title labels, scrollview, pagecontrol)

--- Joomag/Joomag/iPad/controllers/main/ExploreViewController_iPad.m
+++ Joomag/Joomag/iPad/controllers/main/ExploreViewController_iPad.m
@@ -7,8 +7,12 @@
 //
 
 #import "ExploreViewController_iPad.h"
+#import "DataHolder.h"
 
-@interface ExploreViewController_iPad ()
+@interface ExploreViewController_iPad () {
+    DataHolder *dataHolder;
+    CGRect titleLabelsViewFrame, pageControlFrame, scrollViewFrame;
+}
 
 @end
 
@@ -18,6 +22,44 @@
     [super loadView];
     
     NSLog(@"ExploreViewController_iPad");
+    
+    dataHolder = [DataHolder sharedData];
 }
+
+
+
+- (void)viewDidLayoutSubviews {
+    
+    UIInterfaceOrientation iOrientation = [UIApplication sharedApplication].statusBarOrientation;
+    
+    switch(iOrientation)
+    {
+        case UIDeviceOrientationPortrait:
+            
+            titleLabelsViewFrame = CGRectMake(0, 30, 768, 30);
+            pageControlFrame = CGRectMake(0, 30, 768, 30);
+            
+            break;
+            
+        case UIDeviceOrientationLandscapeLeft:
+            scrollViewFrame = CGRectMake(70, 80, 660, 520);
+            titleLabelsViewFrame = CGRectMake(70, 30, 300, 30);
+            pageControlFrame = CGRectMake(70, 600, 610, 30);
+            break;
+            
+        case UIDeviceOrientationLandscapeRight:
+            
+            pageControlFrame = CGRectMake(0, 30, 1024, 30);
+            break;
+            
+        default:
+            break;
+    };
+    
+    titleLabels.frame = titleLabelsViewFrame;
+    scrollView.frame = scrollViewFrame;
+    pageControl.frame = pageControlFrame;
+}
+
 
 @end
