a59aad71bae15101c023e9f653fdcf8d5d23483e
Anatoli-Petrosyants <anatoli.petrosyants@sourcio2.com>
2013-09-09 01:07:09 +0400

    Explore Landscape Right

--- Joomag/Joomag/iPad/controllers/main/ExploreViewController_iPad.m
+++ Joomag/Joomag/iPad/controllers/main/ExploreViewController_iPad.m
@@ -36,37 +36,23 @@
     
     UIInterfaceOrientation iOrientation = [UIApplication sharedApplication].statusBarOrientation;
     
-    switch(iOrientation)
-    {
-        case UIDeviceOrientationPortrait:
-            topBarFrame = CGRectMake(0, 0, 768, 44);
-            topBarTitleLabelFrame = CGRectMake(20, 0, 150, 44);
-            searchBtnFrame = CGRectMake(768-145, 0, 122, 44);
-            scrollViewFrame = CGRectMake(70, 130, 660, 520);
-            titleLabelsViewFrame = CGRectMake(70, 80, 300, 30);
-            pageControlFrame = CGRectMake(70, 650, 610, 30);
-            categoriesTableFrame = CGRectMake(0, 1024-315, 768, 250);
-            
-            break;
-            
-        case UIDeviceOrientationLandscapeLeft:
-            topBarFrame = CGRectMake(0, 0, 1024, 44);
-            topBarTitleLabelFrame = CGRectMake(20, 0, 150, 44);
-            searchBtnFrame = CGRectMake(1024-145, 0, 122, 44);
-            scrollViewFrame = CGRectMake(70, 110, 660, 520);
-            titleLabelsViewFrame = CGRectMake(70, 60, 300, 30);
-            pageControlFrame = CGRectMake(70, 630, 610, 30);
-            categoriesTableFrame = CGRectMake(1024-260, 46, 240, 768);
-            
-            break;
-            
-        case UIDeviceOrientationLandscapeRight:
-            
-            break;
-            
-        default:
-            break;
-    };
+    if (iOrientation == UIDeviceOrientationPortrait) {
+        topBarFrame = CGRectMake(0, 0, 768, 44);
+        topBarTitleLabelFrame = CGRectMake(20, 0, 150, 44);
+        searchBtnFrame = CGRectMake(768-145, 0, 122, 44);
+        scrollViewFrame = CGRectMake(70, 130, 660, 520);
+        titleLabelsViewFrame = CGRectMake(70, 80, 300, 30);
+        pageControlFrame = CGRectMake(70, 650, 610, 30);
+        categoriesTableFrame = CGRectMake(0, 1024-315, 768, 250);
+    } else {
+        topBarFrame = CGRectMake(0, 0, 1024, 44);
+        topBarTitleLabelFrame = CGRectMake(20, 0, 150, 44);
+        searchBtnFrame = CGRectMake(1024-145, 0, 122, 44);
+        scrollViewFrame = CGRectMake(70, 110, 660, 520);
+        titleLabelsViewFrame = CGRectMake(70, 60, 300, 30);
+        pageControlFrame = CGRectMake(70, 630, 610, 30);
+        categoriesTableFrame = CGRectMake(1024-260, 46, 240, 768);
+    }
     
     CGRect frm = CGRectMake(0, -20, self.view.bounds.size.width, self.view.bounds.size.height);
     self.view.frame = frm;
