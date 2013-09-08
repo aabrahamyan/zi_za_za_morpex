44596c9eeb65975680bd68f6b4c671e182aa553a
Anatoli-Petrosyants <anatoli.petrosyants@sourcio2.com>
2013-09-09 01:10:33 +0400

    My Lib Landscape Right

--- Joomag/Joomag/iPad/controllers/main/LibraryViewController_iPad.m
+++ Joomag/Joomag/iPad/controllers/main/LibraryViewController_iPad.m
@@ -30,44 +30,28 @@
     
     UIInterfaceOrientation iOrientation = [UIApplication sharedApplication].statusBarOrientation;
     
-    switch(iOrientation)
-    {
-        case UIDeviceOrientationPortrait:
-            
-            topBarFrame = CGRectMake(0, 0, 768, 44);
-            filterLabelsFrame = CGRectMake(560, 0, 300, 30);
-            loginContainerFrame = CGRectMake(0, 44, 768, 90);
-            loginTextFrame = CGRectMake(20, 10, 350, 70);
-            joomagButtonFrame = CGRectMake(310, 30, 200, 35);
-            orLabelFrame = CGRectMake(520, 30, 20, 30);
-            fbButtonFrame = CGRectMake(550, 5, 200, 35);
-            twitterButtonFrame = CGRectMake(550, 50, 200, 35);
-            scrollViewFrame = CGRectMake(100, 160, 600, 500);
-            
-            break;
-            
-        case UIDeviceOrientationLandscapeLeft:
-            
-            topBarFrame = CGRectMake(0, 0, 1024, 44);
-            filterLabelsFrame = CGRectMake(800, 0, 300, 30);
-            loginContainerFrame = CGRectMake(0, 44, 1024, 90);
-            loginTextFrame = CGRectMake(70, 10, 350, 70);
-            joomagButtonFrame = CGRectMake(510, 30, 200, 35);
-            orLabelFrame = CGRectMake(720, 30, 20, 30);
-            fbButtonFrame = CGRectMake(750, 5, 200, 35);
-            twitterButtonFrame = CGRectMake(750, 50, 200, 35);
-            scrollViewFrame = CGRectMake(100, 160, 850, 500);
-            
-            break;
-            
-        case UIDeviceOrientationLandscapeRight:
-            
-            break;
-            
-        default:
-            break;
-    };
-    
+    if (iOrientation == UIDeviceOrientationPortrait) {
+        topBarFrame = CGRectMake(0, 0, 768, 44);
+        filterLabelsFrame = CGRectMake(560, 0, 300, 30);
+        loginContainerFrame = CGRectMake(0, 44, 768, 90);
+        loginTextFrame = CGRectMake(20, 10, 350, 70);
+        joomagButtonFrame = CGRectMake(310, 30, 200, 35);
+        orLabelFrame = CGRectMake(520, 30, 20, 30);
+        fbButtonFrame = CGRectMake(550, 5, 200, 35);
+        twitterButtonFrame = CGRectMake(550, 50, 200, 35);
+        scrollViewFrame = CGRectMake(100, 160, 600, 500);
+    } else {
+        topBarFrame = CGRectMake(0, 0, 1024, 44);
+        filterLabelsFrame = CGRectMake(800, 0, 300, 30);
+        loginContainerFrame = CGRectMake(0, 44, 1024, 90);
+        loginTextFrame = CGRectMake(70, 10, 350, 70);
+        joomagButtonFrame = CGRectMake(510, 30, 200, 35);
+        orLabelFrame = CGRectMake(720, 30, 20, 30);
+        fbButtonFrame = CGRectMake(750, 5, 200, 35);
+        twitterButtonFrame = CGRectMake(750, 50, 200, 35);
+        scrollViewFrame = CGRectMake(100, 160, 850, 500);
+    }
+
     CGRect frm = CGRectMake(0, -20, self.view.bounds.size.width, self.view.bounds.size.height);
     self.view.frame = frm;
     topBar.frame = topBarFrame;
